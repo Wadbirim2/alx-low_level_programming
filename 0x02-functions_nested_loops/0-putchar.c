67385b61a206d381209f88495c41235d71596797
Amir Abbakar <bainkwe@outlook.com>
2022-12-13 13:59:33 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -entry point
+ * return:0
+ */
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+	while (i<8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	 _putchar('\n');
+	 return(0);
+}
