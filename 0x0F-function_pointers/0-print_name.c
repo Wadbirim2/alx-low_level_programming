499ee887e91d57668eb6a412357fbeb1d140b3fc
Amir Abbakar <bainkwe@outlook.com>
2023-01-19 12:50:38 -0800

    Beauty is variable, ugliness is constant

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include "holberton.h"
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
+ * print_name_as_is - prints a name as is
  * @name: name of the person
- * @f: input pointer function and execute it
- * Return: nothing
+ *
+ * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != 0 && f != 0)
 		f(name);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_name("Bob", print_name);
+        printf("\n");
+    return (0);
 }
