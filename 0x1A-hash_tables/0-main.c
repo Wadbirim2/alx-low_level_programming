059adc07448044b49b342af5bbbb9fc3b607ed87
Amir Abbakar <bainkwe@outlook.com>
2023-04-07 16:30:39 -0700

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
+julien@ubuntu:~/0x1A. Hash tables$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-hash_table_create.c -o a
---
+++ 0x1A-hash_tables/6-main.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+    char *key;
+    char *value;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Streetz");
+    key = strdup("Tim");
+    value = strdup("Britton");
+    hash_table_set(ht, key, value);
+    key[0] = '\0';
+    value[0] = '\0';
+    free(key);
+    free(value);
+    hash_table_set(ht, "98", "Battery Street"); 
+    hash_table_set(ht, "hetairas", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z");
+    hash_table_set(ht, "mentioner", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z Chu");
+    hash_table_print(ht);
+    hash_table_delete(ht);
+    return (EXIT_SUCCESS);
+}
+julien@ubuntu:~/0x1A. Hash tables$ gcc -Wall -pedantic -Werror -Wextra 6-main.c 0-hash_table_create.c 1-djb2.c 2-key_index.c 3-hash_table_set.c 4-hash_table_get.c 5-hash_table_print.c 6-hash_table_delete.c -o g
