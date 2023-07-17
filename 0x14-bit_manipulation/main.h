b564bedf5a8f0fe27e9d303dc28c70a829fc14ca
Wadbirim2 <bainkwe@outlook.com>
2023-07-16 21:54:34 -0700

    0.0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,18 +1,13 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
